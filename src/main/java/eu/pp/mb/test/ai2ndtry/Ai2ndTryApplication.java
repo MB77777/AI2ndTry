d244de762c7cc3be0f45c486ab6e120e2c11d66d
MB77777 <jehac4s@gmail.com>
2026-04-25 23:17:28 +0200

    0.0.1 - Init

---
+++ src/main/java/eu/pp/mb/test/ai2ndtry/Ai2ndTryApplication.java
@@ -0,0 +1,13 @@
+package eu.pp.mb.test.ai2ndtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ai2ndTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ai2ndTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/pp/mb/test/ai2ndtry/Ai2ndTryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.pp.mb.test.ai2ndtry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ai2ndTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
